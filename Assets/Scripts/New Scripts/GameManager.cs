5b8e3dfef103bff424fc5a5838a43c38179c127f
TLSimmons42 <TrentNeuro42@gmail.com>
2021-11-16 01:24:29 -0500

    New Game Manager

---
+++ Assets/Scripts/New Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doozy.Engine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float cubeDropTimer = .5f;
+    public GameObject[] PlaywallDropPoints;
+
+    public GameObject BlueCube;
+    public GameObject RedCube;
+    public GameObject GoldCube;
+    public GameObject NeutralCube;
+
+    public GameObject buildWall1;
+    public GameObject buildWall2;
+    public GameObject ViewWall1;
+    public GameObject ViewWall2;
+
+    public UIView startView;
+    public UIView waitingView;
+    public UIView gameOverView;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    
+    // this will be called at the start of the game to build a the view wall for the player
+    public void BuildViewWall(int difficulty)
+    {
+
+    }
+
+
+    // this will be called durring the game in order to build a new cube on the Play Wall
+    public void BuildNewCube()
+    {
+        
+    }
+}
