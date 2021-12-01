77995879938008ef3f8e9df3e65f001f16a16891
Nate Sawant <74106957+natesawant@users.noreply.github.com>
2021-12-01 17:37:41 -0500

    Implement main menu functionality

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public void LaunchGame()
+    {
+        SceneManager.LoadScene("Integration Main");
+    }
     public void ChangeToOnePlayer()
     {
         PlayerPrefs.SetInt("playerCount", 1);
+        ChangeToPlayerOneChoice();
     }
 
     public void ChangeToTwoPlayer()
@@ -14,5 +20,41 @@ public class MainMenuScript : MonoBehaviour
         PlayerPrefs.SetInt("playerCount", 2);
     }
 
+    public void ChangeToEasy()
+    {
+        PlayerPrefs.SetString("gameDifficulty", "easy");
+    }
+    public void ChangeToMedium()
+    {
+        PlayerPrefs.SetString("gameDifficulty", "medium");
+    }
+    public void ChangeToHard()
+    {
+        PlayerPrefs.SetString("gameDifficulty", "hard");
+    }
 
+    public void ChangeToPlayerOneChoice()
+    {
+        PlayerPrefs.SetString("playerChoice", "P1");
+    }
+
+    public void ChangeToPlayerTwoChoice()
+    {
+        PlayerPrefs.SetString("playerChoice", "P2");
+    }
+
+    public void ChangeToMouse()
+    {
+        PlayerPrefs.SetString("player head", "mouse");
+    }
+
+    public void ChangeToHuman()
+    {
+        PlayerPrefs.SetString("player head", "human");
+    }
+
+    public void ChangeToCat()
+    {
+        PlayerPrefs.SetString("player head", "cat");
+    }
 }
