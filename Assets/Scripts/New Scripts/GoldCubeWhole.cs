e51074cfc9771272899c98e94a948661c2ddbe2f
Trent Simmons <trentneuro42@gmail.com>
2022-03-03 14:55:48 -0500

    Update GoldCubeWhole.cs

--- Assets/Scripts/New Scripts/GoldCubeWhole.cs
+++ Assets/Scripts/New Scripts/GoldCubeWhole.cs
@@ -78,12 +78,14 @@ public class GoldCubeWhole : XRSimpleInteractable
         {
             if (interactor.transform.parent.parent.gameObject.tag == "P1")
             {
+                Debug.Log("spawning left half");
                 GameObject cube = PhotonNetwork.Instantiate("Network Gold Left Half", transform.position, Quaternion.identity);
                 cube.GetComponent<XRGrabNetworkInteractable>().currentZone = cube.GetComponent<XRGrabNetworkInteractable>().NoZone;
                 PhotonNetwork.Destroy(gameObject);
             }
             else if (interactor.transform.parent.parent.gameObject.tag == "P2")
             {
+                Debug.Log("spawning right half");
                 GameObject cube = PhotonNetwork.Instantiate("Network Gold Right Half", transform.position, Quaternion.identity);
                 cube.GetComponent<XRGrabNetworkInteractable>().currentZone = cube.GetComponent<XRGrabNetworkInteractable>().NoZone;
                 PhotonNetwork.Destroy(gameObject);
