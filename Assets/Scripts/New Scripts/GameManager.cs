77bdbaf09777d7cf11efa600fdb8a9129d6ba79a
Trent Simmons <trentneuro42@gmail.com>
2022-03-10 19:06:43 -0500

    testing

--- Assets/Scripts/New Scripts/GameManager.cs
+++ Assets/Scripts/New Scripts/GameManager.cs
@@ -287,7 +287,9 @@ public class GameManager : Singleton<GameManager>
                 }
                 if (cubeChoice == 2)
                 {
-                    cube = PhotonNetwork.Instantiate("Network Gold Cube", PlaywallDropPoints[spawnPointChoice].transform.position, Quaternion.identity);
+                    //cube = PhotonNetwork.Instantiate("Network Gold Cube", PlaywallDropPoints[spawnPointChoice].transform.position, Quaternion.identity);
+                    cube = PhotonNetwork.Instantiate("Gold Parent", PlaywallDropPoints[spawnPointChoice].transform.position, Quaternion.identity);
+
                 }
                 if (cubeChoice == 3)
                 {
