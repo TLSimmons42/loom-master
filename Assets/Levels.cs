9437a772a81535a7af341ed906b5bd11eb6dba43
Nate Sawant <74106957+natesawant@users.noreply.github.com>
2022-03-31 15:48:48 -0400

    First commit

---
+++ Assets/Levels.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Array2DEditor;
+
+public class Levels : Singleton<Levels>
+{
+    
+
+    List<Array2DString> easyLevels = new List<Array2DString>();
+    [Header("Easy Levels")]
+
+    [SerializeField]
+    public Array2DString easy1;
+    [SerializeField]
+    public Array2DString easy2;
+
+    
+
+    List<Array2DString> mediumLevels = new List<Array2DString>();
+    [Header("Medium Levels")]
+
+    [SerializeField]
+    public Array2DString medium1;
+    [SerializeField]
+    public Array2DString medium2;
+
+    List<Array2DString> hardLevels = new List<Array2DString>();
+    [Header("Hard Levels")]
+
+    [SerializeField]
+    public Array2DString hard1;
+    [SerializeField]
+    public Array2DString hard2;
+
+    public void Start()
+    {
+        initializeLevels();
+    }
+
+    public void initializeLevels()
+    {
+        easyLevels.Add(easy1);
+        easyLevels.Add(easy2);
+
+        mediumLevels.Add(medium1);
+        mediumLevels.Add(medium2);
+
+        hardLevels.Add(hard1);
+        hardLevels.Add(hard2);
+    }
+
+    public string[,] getRandomLevel(string difficulty)
+    {
+        switch (difficulty) 
+        {
+            case "easy":
+                return easyLevels[Random.Range(0, easyLevels.Count)].GetCells();
+            case "medium":
+                return mediumLevels[Random.Range(0, mediumLevels.Count)].GetCells();
+            case "hard":
+                return hardLevels[Random.Range(0, hardLevels.Count)].GetCells();
+            default:
+                return easyLevels[Random.Range(0, easyLevels.Count)].GetCells();
+        }
+    }
+}
---
+++ Assets/MasterBuildWall.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Array2DEditor;
+using Photon.Pun;
+
+public class MasterBuildWall : Singleton<MasterBuildWall>
+{
+    [SerializeField]
+    private Array2DString editorBuildArray;
+
+    private string[,] levelImport;
+    private string[,] targetWall;
+    private string[,] masterBuildArray = new string[5,5];
+
+    public GameObject dropZone;
+
+    public GameObject redCube, blueCube, clearCube, goldCube, leftGoldCube, rightGoldCube;
+
+
+    public void Start()
+    {
+        initBuildWallTest();
+        displayEditorArray();
+    }
+
+    public void importLevel()
+    {
+        levelImport = Levels.instance.getRandomLevel(PlayerPrefs.GetString("gameDifficulty"));
+    }
+
+    public void initalizeDropZones()
+    {
+
+    }
+
+    public void setTargetWall()
+    {
+        targetWall = new string[levelImport.GetLength(0) - 2, levelImport.GetLength(1) - 2];
+
+        for (int i = 1; i < levelImport.GetLength(0) - 1; i++)
+        {
+            for (int j = 1; j < levelImport.GetLength(1) - 1; j++)
+            {
+                targetWall[i - 1, j - 1] = levelImport[i, j];
+            }
+        }
+    }
+
+    public void initializeBuildWalls()
+    {
+
+    }
+
+    private void initBuildWallTest()
+    {
+        masterBuildArray = new string[5, 5]
+        {
+            {"a", "b", "c", "d", "e" },
+            {"f", "g", "h", "i", "j" },
+            {"k", "l", "m", "n", "o" },
+            {"p", "q", "r", "s", "t" },
+            {"u", "v", "w", "x", "y" }
+        };
+    }
+
+    private void displayEditorArray()
+    {
+        for (int x = 0; x < masterBuildArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < masterBuildArray.GetLength(1); y++)
+            {
+                Debug.Log("X:" + x + ", Y:" + y);
+                Debug.Log(masterBuildArray[x, y]);
+                editorBuildArray.SetCell(y, x, masterBuildArray[x, y]);
+            }
+        }
+    }
+
+    public GameObject cubeCodeToGameObject(string cubeCode)
+    {
+        switch (cubeCode)
+        {
+            case "R":
+                return redCube;
+            case "B":
+                return blueCube;
+            case "C":
+                return clearCube;
+            case "G":
+                return goldCube;
+            case "lG":
+                return leftGoldCube;
+            case "rG":
+                return rightGoldCube;
+            default:
+                Debug.LogError("Invalid cubeCode: " + cubeCode);
+                return null;
+        }
+    }
+
+    public string gameObjectToCubeCode(GameObject cube)
+    {
+        switch (cube.tag)
+        {
+            case "red cube":
+                return "R";
+            case "blue cube":
+                return "B";
+            case "invis cube":
+                return "C";
+            case "gold cube":
+                return "G";
+            case "left gold cube":
+                return "lG";
+            case "right gold cube":
+                return "rG";
+            default:
+                Debug.LogError("Invalid cube: " + cube.tag);
+                return null;
+        }
+    }
+
+    [PunRPC]
+
+    public void addCube(int x, int y, string cubeCode)
+    {
+
+    }
+
+    [PunRPC]
+
+    public void removeCube(int x, int y, string cubeCode)
+    {
+
+    }
+}
